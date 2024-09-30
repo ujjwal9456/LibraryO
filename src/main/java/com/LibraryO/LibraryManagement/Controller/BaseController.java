9e400aaa43c3a5bbbabb40564f115f44cfd2453c
ujjwal9456 <ujjwalshekhar123@gmail.com>
2024-09-30 19:29:17 +0530

    Added support for postgresql

--- src/main/java/com/LibraryO/LibraryManagement/Controller/BaseController.java
+++ src/main/java/com/LibraryO/LibraryManagement/Controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.LibraryO.LibraryManagement.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/")
 public class BaseController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping(path = "/Ping")
     public String GetAliveStatus(){
+
+        String sql = "INSERT INTO public.\"Library\" (libraryname) VALUES ('AnotherLibrary')";
+
+        int rows = jdbcTemplate.update(sql);
+        if (rows > 0) {
+            System.out.println("A new row has been inserted.");
+        }
+
+
+
         return "Yes I am up";
     }
 
--- src/main/java/com/LibraryO/LibraryManagement/LibraryManagementApplication.java
+++ src/main/java/com/LibraryO/LibraryManagement/LibraryManagementApplication.java
@@ -1,10 +1,13 @@
 package com.LibraryO.LibraryManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class LibraryManagementApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
